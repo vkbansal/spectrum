b8e8fb9e715468fb76a3d66805d42cc9de6522af
Vivek Kumar Bansal <vkb0310@gmail.com>
2014-12-21 22:41:44 +0530

    Added CoffeeScript

---
+++ src/Languages/Definitions/CoffeeScript.php
@@ -0,0 +1,87 @@
+<?php
+namespace VKBansal\Prism\Languages\Definitions;
+
+use VKBansal\Prism\Languages\AbstractLanguage;
+
+class CoffeeScript extends AbstractLanguage
+{
+    public function definition()
+    {
+        return $this->extend('javascript', [
+            // Ignore comments starting with { to privilege string interpolation highlighting
+            'comment'=> "/#(?!\{).+/",
+            'string'=> [
+                // Strings are multiline
+                "/'(?:\\\\?[\s\S])*?'/",
+                [
+                    // Strings are multiline
+                    "pattern"=> "/\"(?:\\\\?[\s\S])*?\"/",
+                    "inside"=> [
+                        'interpolation'=> [
+                            "pattern"=> "/\#\{[^}]+\}/",
+                            "alias"=> 'variable'
+                        ]
+                    ]
+                ]
+            ],
+            'keyword'=> "/\b(and|break|by|catch|class|continue|debugger|delete|do|each|else|extend|extends|false|finally|for|if|in|instanceof|is|isnt|let|loop|namespace|new|no|not|null|of|off|on|or|own|return|super|switch|then|this|throw|true|try|typeof|undefined|unless|until|when|while|window|with|yes|yield)\b/",
+            'class-member'=> [
+                "pattern"=> "/@(?!\d)\w+/",
+                "alias"=> 'variable'
+            ]
+        ]);
+    }
+
+    public function setup()
+    {
+        $comment = $this->repository->getDefinition('coffeescript.comment');
+        $interpolation = $this->repository->getDefinition('coffeescript.string')[1]['inside']['interpolation'];
+
+        $this->insertBefore('coffeescript', [
+            'multiline-comment'=> [
+                "pattern"=> "/\#\#\#[\s\S]+?\#\#\#/",
+                "alias"=> 'comment'
+            ],
+            // Block regexp can contain comments and interpolation
+            'block-regex'=> [
+                "pattern"=> "/\/{3}[\s\S]*?\/{3}/",
+                "alias"=> 'regex',
+                "inside"=> [
+                    'comment'=> $comment,
+                    'interpolation'=> $interpolation
+                ]
+            ]
+        ], 'comment');
+
+        $this->insertBefore('coffeescript', [
+            'inline-javascript'=> [
+                "pattern"=> "/`(?:\\\\?[\s\S])*?`/",
+                "inside"=> [
+                    'delimiter'=> [
+                        "pattern"=> "/^`|`$/",
+                        "alias"=> 'punctuation'
+                    ],
+                    "rest"=> $this->repository->getDefinition('javascript')
+                ]
+            ],
+            'multiline-string'=> [ // Block strings
+                [
+                    "pattern"=> "/'''[\s\S]*?'''/",
+                    "alias"=> 'string'
+                ],
+                [
+                    "pattern"=> '/"""[\s\S]*?"""/',
+                    "alias"=> 'string',
+                    "inside"=> [
+                        "interpolation"=> $interpolation
+                    ]
+                ]
+            ]
+
+        ], 'string');
+
+        $this->insertBefore('coffeescript', [
+            'property'=> "/(?!\d)\w+(?=\s*:(?!:))/" // Object property
+        ], 'keyword');
+    }
+}
--- src/Languages/map.php
+++ src/Languages/map.php
@@ -8,25 +8,28 @@
  */
 return [
     'map' => [
-        'bash'       => 'VKBansal\Prism\Languages\Definitions\Bash',
-        'c'          => 'VKBansal\Prism\Languages\Definitions\CLang',
-        'clike'      => 'VKBansal\Prism\Languages\Definitions\CLike',
-        'css'        => 'VKBansal\Prism\Languages\Definitions\CSS',
-        'ini'        => 'VKBansal\Prism\Languages\Definitions\Ini',
-        'java'       => 'VKBansal\Prism\Languages\Definitions\Java',
-        'javascript' => 'VKBansal\Prism\Languages\Definitions\JavaScript',
-        'less'       => 'VKBansal\Prism\Languages\Definitions\Less',
-        'markup'     => 'VKBansal\Prism\Languages\Definitions\Markup',
-        'php'        => 'VKBansal\Prism\Languages\Definitions\Php',
-        'python'     => 'VKBansal\Prism\Languages\Definitions\Python',
-        'ruby'       => 'VKBansal\Prism\Languages\Definitions\Ruby',
-        'sql'        => 'VKBansal\Prism\Languages\Definitions\Sql'
+        'bash'         => 'VKBansal\Prism\Languages\Definitions\Bash',
+        'c'            => 'VKBansal\Prism\Languages\Definitions\CLang',
+        'clike'        => 'VKBansal\Prism\Languages\Definitions\CLike',
+        'coffeescript' => 'VKBansal\Prism\Languages\Definitions\CoffeeScript',
+        'css'          => 'VKBansal\Prism\Languages\Definitions\CSS',
+        'ini'          => 'VKBansal\Prism\Languages\Definitions\Ini',
+        'java'         => 'VKBansal\Prism\Languages\Definitions\Java',
+        'javascript'   => 'VKBansal\Prism\Languages\Definitions\JavaScript',
+        'less'         => 'VKBansal\Prism\Languages\Definitions\Less',
+        'markup'       => 'VKBansal\Prism\Languages\Definitions\Markup',
+        'php'          => 'VKBansal\Prism\Languages\Definitions\Php',
+        'python'       => 'VKBansal\Prism\Languages\Definitions\Python',
+        'ruby'         => 'VKBansal\Prism\Languages\Definitions\Ruby',
+        'sql'          => 'VKBansal\Prism\Languages\Definitions\Sql'
     ],
     
     'aliases'=> [
-        'html' => 'markup',
-        'js'   => 'javascript',
-        'xml'  => 'markup'
+        'coffee'        => 'coffeescript',
+        'coffee-script' => 'coffeescript',
+        'html'          => 'markup',
+        'js'            => 'javascript',
+        'xml'           => 'markup'
     ],
 
     'defaults' => [ 'markup', 'clike', 'css', 'javascript', 'php', 'bash' ]
