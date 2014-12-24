60cbceaeabb43cf797631b5e4e9135fdadef532a
Vivek Kumar Bansal <vkb0310@gmail.com>
2014-12-24 16:41:12 +0530

    CS Fixes

--- src/Plugin/PluginInterface.php
+++ src/Plugin/PluginInterface.php
@@ -3,5 +3,10 @@ namespace VKBansal\Prism\Plugin;
 
 interface PluginInterface
 {
+    /**
+     * Plugin definition as a closure. 
+     * Closure will be bound to VKBansal\Prism\Prism.
+     * @return \Closure
+     */
     public function handle();
 }
