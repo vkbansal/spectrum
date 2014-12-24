54d582581ae94b4f64066267b519e28d41666c67
Vivek Kumar Bansal <vkb0310@gmail.com>
2014-12-24 16:44:00 +0530

    fix typo

--- src/Plugin/PluggableTrait.php
+++ src/Plugin/PluggableTrait.php
@@ -8,7 +8,7 @@ trait PluggableTrait
     public function addPlugin(PluginInterface $plugin)
     {
         $handle = $plugin->handle();
-        if (!is_callable($handle) || ! $handle instacneof \Closure) {
+        if (!is_callable($handle) || ! $handle instanceof \Closure) {
             throw new \Exception("handle must return a closure");
         }
         $handle = $handle->bindTo($this);
