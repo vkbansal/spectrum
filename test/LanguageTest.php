3d68be73c99c3296b1318d8dc7659b70a7b43a56
Vivek Kumar Bansal <vkb0310@gmail.com>
2014-12-25 12:36:38 +0530

    Updated tests

--- test/LanguageTest.php
+++ test/LanguageTest.php
@@ -1,30 +1,28 @@
 <?php
 
-use VKBansal\Prism\Prism;
-
 class LanguageTest extends PHPUnit_Framework_TestCase {
 
     protected $repo;
     
     public function setUp()
     {
-        //$this->repo = new Prism();
+        $this->repo = new \VKBansal\Prism\AssetManager();
     }
 
     public function testInsertBefore()
     {
-        //$this->repo->loadDefinition('markup');
-        //$this->repo->loadDefinition('css');
-        //$this->assertTrue(isset($this->repo->getDefinition('markup')['style']));
-        //$this->assertTrue(isset($this->repo->getDefinition('markup.tag.inside')['style-attr']));
+        $this->repo->loadDefinition('markup');
+        $this->repo->loadDefinition('css');
+        $this->assertTrue(isset($this->repo->getDefinition('markup')['style']));
+        $this->assertTrue(isset($this->repo->getDefinition('markup.tag.inside')['style-attr']));
     }
 
     public function testExtend()
     {
-        //$this->repo->loadDefinition('clike');
-        //$this->repo->loadDefinition('javascript');
-        //$this->assertTrue(isset($this->repo->getDefinition('javascript')['comment']));
-        //$this->assertTrue(isset($this->repo->getDefinition('javascript')['regex']));
+        $this->repo->loadDefinition('clike');
+        $this->repo->loadDefinition('javascript');
+        $this->assertTrue(isset($this->repo->getDefinition('javascript')['comment']));
+        $this->assertTrue(isset($this->repo->getDefinition('javascript')['regex']));
     }
     
 }
---
+++ test/resources/GeneratorTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use VKBansal\Prism\Token\Generator;
+
+class GeneratorTest extends PHPUnit_Framework_TestCase
+{
+    protected $generator;
+
+    public function setUp(){
+        $grammar = [
+            'rest' => [
+                'token1' => [
+                    'pattern' => '/dummy text/',
+                    'inside' => [
+                        'token2' => '/text/'
+                    ]
+                ]
+            ]
+        ];
+        $this->generator = new Generator('dummy text', $grammar, 'lang');
+    }
+
+    public function testGenerate(){
+        $nodes = $this->generator->generate();
+        $token = $nodes[0];
+        $this->assertInstanceOf('VKBansal\Prism\Token\Token', $token);
+        $this->assertInstanceOf('VKBansal\Prism\Token\Token', $token->content[1]);
+    }
+
+    public function testToNodes()
+    {
+        $document = new DOMDocument();
+
+        $this->generator->generate();
+        $node = $this->generator->toNodes($document);
+        $html = $node[0]->ownerDocument->saveHTML($node[0]);
+
+        $this->assertEquals('<span class="token token1">dummy <span class="token token2">text</span></span>', $html);
+
+    }
+}
