4e6a28eed74b8cde7f3e8ec9b1f5e595874cf494
Vivek Kumar Bansal <vkb0310@gmail.com>
2015-01-16 16:28:12 +0530

    Refactored utility methods

--- test/UtilTest.php
+++ test/UtilTest.php
@@ -3,50 +3,102 @@ use VKBansal\Prism\Util;
 
 class UtilTest extends PHPUnit_Framework_TestCase {
 
-    public function testIsAssoc(){
+    public function testIsAssoc()
+    {
         $this->assertFalse(Util::isAssoc([1, 2]));
         $this->assertTrue(Util::isAssoc(['1' => '2', 'test' => '123']));
     }
-    
-    public function testHtmlEncode(){
+
+    public function testHtmlEncode()
+    {
         $encoded = file_get_contents(__DIR__."/resources/htmlEncoded.html");
         $decoded = file_get_contents(__DIR__."/resources/htmlDecoded.html");
 
         $this->assertEquals($encoded, Util::encodeCodeBlocks($decoded));
     }
 
-    public function testHtmlDecode(){
+    public function testHtmlDecode()
+    {
         $encoded = file_get_contents(__DIR__."/resources/htmlEncoded.html");
         $decoded = file_get_contents(__DIR__."/resources/htmlDecoded.html");
 
         $this->assertEquals($decoded, Util::decodeCodeBlocks($encoded));
     }
 
-    public function testDFS(){
-        $arr = [
-            'key1' => 'one',
-            'key2' => ['two' , '2', 'too'],
-            'key3' => [ 
-                'a' => '123456',
-                'b' => '987654'
-            ]
-        ];
+    public function testGetInnerHtml()
+    {
+        $doc = new DOMDocument();
+        $elem = $doc->createElement("div", "Test content");
 
-        $arr2 = [
-            'key1' => 'onetest',
-            'key2' => ['twotest' , '2test', 'tootest'],
-            'key3' => [ 
-                'a' => '123456test',
-                'b' => '987654test'
-            ]
-        ];
-
-        array_walk_recursive($arr, function(&$value, $key){
-            if(is_string($value)) {
-                $value .="test";
-            }
-            //var_dump($obj);
-        });
-        $this->assertEquals($arr2, $arr);
+        $this->assertEquals("Test content", Util::getInnerHtml($elem));
     }
+
+    public function testSetInnerHtml()
+    {
+        $doc = new DOMDocument();
+        $elem = $doc->createElement("div");
+
+        $nodes = [
+            $doc->createTextNode("Test"),
+            $doc->createTextNode(" content")
+        ];
+
+        Util::setInnerHTML($elem, $nodes);
+
+        $this->assertEquals("Test content", Util::getInnerHtml($elem));
+    }
+
+    public function testAddClass()
+    {
+        $doc = new DOMDocument();
+        $elem = $doc->createElement("div");
+
+        Util::addClass($elem, "test");
+
+        $this->assertEquals("test", $elem->getAttribute('class'));
+    }
+
+    public function testRemoveClassString()
+    {
+        $doc = new DOMDocument();
+        $elem = $doc->createElement("div");
+
+        Util::addClass($elem, "myclass language-test dummy test");
+        Util::removeClass($elem, "myclass dummy");
+
+        $this->assertEquals("language-test test", $elem->getAttribute('class'));
+
+    }
+
+    public function testRemoveClassRegex()
+    {
+        $doc = new DOMDocument();
+        $elem = $doc->createElement("div");
+
+        Util::addClass($elem, "myclass language-test dummy test");
+        Util::removeClass($elem, "/\blang(?:uage)?-(?!\*)(\w+)\b/i", true);
+
+        $this->assertEquals("myclass dummy test", $elem->getAttribute('class'));
+    }
+
+    public function testDetectLanguage()
+    {
+        $doc = new DOMDocument();
+        $elem = $doc->createElement("div");
+
+        Util::addClass($elem, "myclass language-test dummy test");
+        $this->assertEquals("test", Util::detectLanguage($elem));
+
+        Util::removeClass($elem, "language-test");
+        $this->assertEquals("", Util::detectLanguage($elem));
+    }
+
+    public function testIsPre()
+    {
+        $doc = new DOMDocument();
+        $elem = $doc->createElement("PRE");
+
+        $this->assertTrue(Util::isPre($elem));
+    }
+
 }
