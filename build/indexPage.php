f902f5393530e3d18ea210a02c4f7950fb79183c
Vivek Kumar Bansal <vkb0310@gmail.com>
2015-01-26 14:02:46 +0530

    updated docs

--- build/indexPage.php
+++ build/indexPage.php
@@ -2,7 +2,7 @@
 /**************************************************************
  * Index page
  *************************************************************/
-$txt = file_get_contents(__DIR__.'/../../prism.php/README.md');
+$txt = file_get_contents(__DIR__.'/../index.md');
 $txt = $mark->text($txt);
 $txt = $twig->render('index.twig', ['content'=> $txt, 'active' => 'home', 'base' => $base]);
 $console->out("<light_green>Building</light_green> <bold><light_cyan>Home Page<light_cyan></bold>");
