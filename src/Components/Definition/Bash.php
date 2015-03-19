dd672d3b3dfc352178cbe90fbc3829a4ebe255bf
Vivek Kumar Bansal <vkb0310@gmail.com>
2015-03-19 15:07:31 +0530

    Bash: update def

--- src/Components/Definition/Bash.php
+++ src/Components/Definition/Bash.php
@@ -29,13 +29,19 @@ class Bash extends AbstractDefinition
             ],
             'string'=> [
                 //allow multiline string
-                "pattern"=> "/(\"|')(\\\\?[\s\S])*?\g{1}/",
+                "pattern"=> "/(\"|')(\\\\?[\\s\\S])*?\\g{1}/",
                 "inside"=> [
                     //'property' class reused for bash variables
-                    'property'=> "/\\\$([a-zA-Z0-9_#\?\-\*!@]+|\{[^\}]+\})/"
+                    'property'=> "/\\\$([a-zA-Z0-9_#\\?\\-\\*!@]+|\\{[^\\}]+\\})/"
                 ]
             ],
-            'keyword' => "/\b(if|then|else|elif|fi|for|break|continue|while|in|case|function|select|do|done|until|echo|exit|return|set|declare)\b/"
+            // Redefined to prevent highlighting of numbers in filenames
+            'number'=> [
+                "pattern" => "/([^\\w\\.])-?(0x[\\dA-Fa-f]+|\\d*\\.?\\d+([Ee]-?\\d+)?)\\b/",
+                "lookbehind" => true
+            ],
+            'function' => "/\\b(?:alias|apropos|apt-get|aptitude|aspell|awk|basename|bash|bc|bg|builtin|bzip2|cal|cat|cd|cfdisk|chgrp|chmod|chown|chroot|chkconfig|cksum|clear|cmp|comm|command|cp|cron|crontab|csplit|cut|date|dc|dd|ddrescue|declare|df|diff|diff3|dig|dir|dircolors|dirname|dirs|dmesg|du|echo|egrep|eject|enable|env|ethtool|eval|exec|exit|expand|expect|export|expr|fdformat|fdisk|fg|fgrep|file|find|fmt|fold|format|free|fsck|ftp|fuser|gawk|getopts|git|grep|groupadd|groupdel|groupmod|groups|gzip|hash|head|help|hg|history|hostname|htop|iconv|id|ifconfig|ifdown|ifup|import|install|jobs|join|kill|killall|less|link|ln|locate|logname|logout|look|lpc|lpr|lprint|lprintd|lprintq|lprm|ls|lsof|make|man|mkdir|mkfifo|mkisofs|mknod|more|most|mount|mtools|mtr|mv|mmv|nano|netstat|nice|nl|nohup|notify-send|nslookup|open|op|passwd|paste|pathchk|ping|pkill|popd|pr|printcap|printenv|printf|ps|pushd|pv|pwd|quota|quotacheck|quotactl|ram|rar|rcp|read|readarray|readonly|reboot|rename|renice|remsync|rev|rm|rmdir|rsync|screen|scp|sdiff|sed|select|seq|service|sftp|shift|shopt|shutdown|sleep|slocate|sort|source|split|ssh|stat|strace|su|sudo|sum|suspend|sync|tail|tar|tee|test|time|timeout|times|touch|top|traceroute|trap|tr|tsort|tty|type|ulimit|umask|umount|unalias|uname|unexpand|uniq|units|unrar|unshar|until|uptime|useradd|userdel|usermod|users|uuencode|uudecode|v|vdir|vi|vmstat|wait|watch|wc|wget|whereis|which|who|whoami|write|xargs|xdg-open|yes|zip)\\b/",
+            'keyword' => "/\\b(if|then|else|elif|fi|for|break|continue|while|in|case|function|select|do|done|until|echo|exit|return|set|declare)\\b/"
         ]);
     }
 
@@ -46,12 +52,12 @@ class Bash extends AbstractDefinition
     {
         $this->insertBefore('bash', [
             //'property' class reused for bash variables
-            "property"=> "/\\\$([a-zA-Z0-9_#\?\-\*!@]+|\{[^}]+\})/"
+            "property"=> "/\\\$([a-zA-Z0-9_#\\?\\-\\*!@]+|\\{[^}]+\\})/"
         ], 'keyword');
 
         $this->insertBefore('bash', [
             //shebang must be before comment, 'important' class from css reused
-            'important'=> "/(^#!\s*\/bin\/bash)|(^#!\s*\/bin\/sh)/"
+            'important'=> "/(^#!\\s*\\/bin\\/bash)|(^#!\\s*\\/bin\\/sh)/"
         ], 'comment');
     }
 }
